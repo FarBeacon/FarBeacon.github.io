9967f9f18a9c1f8e1607f36d2a0433a5542de0dd
diemonic1 <diemonic1@gmail.com>
2024-01-30 23:11:00 +0300

    LanguageUpdate

---
+++ language.js
@@ -0,0 +1,13 @@
+function EN(){
+    localStorage.setItem(
+        "language", "EN"
+    );
+    location.reload();
+}
+
+function RU() {
+    localStorage.setItem(
+        "language", "RU"
+    );
+    location.reload();
+}

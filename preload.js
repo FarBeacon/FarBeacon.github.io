747b0ab4523ee374a035d693fd50d0fece68bb71
diemonic1 <diemonic1@gmail.com>
2024-01-31 01:32:35 +0300

    animation

---
+++ preload.js
@@ -0,0 +1,8 @@
+setTimeout(() => {  
+    var elements = document.getElementsByClassName("fadeInUp");
+
+    for (var i = 0; i < elements.length; i++) {
+        console.log(elements[i]);
+        elements[i].style.display = 'block';
+    }
+}, 300);
